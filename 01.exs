f97e0c62a8b3996e52466bdb98fbbf4f4f499f29
Roger Nesbitt <roger@seriousorange.com>
2018-12-01 20:45:03 +1300

    day 1

---
+++ 01.exs
@@ -0,0 +1,26 @@
+defmodule Day01 do
+  def run do
+    changes = IO.read(:all)
+    |> String.trim
+    |> String.split("\n")
+    |> Enum.map(&String.to_integer/1)
+
+    changes |> add |> IO.puts
+    changes |> find_duplicate_frequency |> IO.puts
+  end
+
+  defp add(changes) do
+    Enum.reduce(changes, 0, &+/2)
+  end
+
+  defp find_duplicate_frequency(changes) do
+    changes
+    |> Stream.cycle
+    |> Stream.scan(0, &+/2)
+    |> Enum.reduce_while(MapSet.new([0]), fn value, history ->
+      if MapSet.member?(history, value), do: {:halt, value}, else: {:cont, MapSet.put(history, value)}
+    end)
+  end
+end
+
+Day01.run
